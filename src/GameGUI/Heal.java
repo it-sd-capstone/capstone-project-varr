6aa857a5e146bc9e915f0979a7eeb69eb0be30b4
Nhia Vuegit config --global user.name Nhia <nvue6@student.cvtc.edu>
2024-05-09 18:15:18 -0500

    Add healing mechanic

--- src/GameGUI/Heal.java
+++ src/GameGUI/Heal.java
@@ -1,2 +1,24 @@
-package GameGUI;public class Heal {
+package GameGUI;
+
+import edu.cvtc.varr.Character;
+import edu.cvtc.varr.Dungeon;
+
+public class Heal {
+
+    private Dungeon dungeon = new Dungeon(3,4);
+
+    public void healPlayer(Character player) {
+        int healAmount = 10;
+        int currentLevel = dungeon.getLevel();
+        healAmount = healAmount + (currentLevel * 10);
+        int newHealth = player.getHealth() + healAmount;
+        if (newHealth > player.getMaxHealth()) {
+            player.setHealth(player.getMaxHealth());
+        } else {
+            player.setHealth(newHealth);
+        }
+        System.out.println("You have been healed for " + healAmount + " health.");
+        System.out.println("Your current health is " + player.getHealth() + ".");
+    }
+
 }
